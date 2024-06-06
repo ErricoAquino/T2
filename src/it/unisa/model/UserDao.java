d40df9a5d7a284afa323255d76b78ba179840a1b
ErricoAquino <errico.aquino24@gmail.com>
2024-06-06 14:55:41 +0200

    ID 6 quasi risolto

--- src/it/unisa/model/UserDao.java
+++ src/it/unisa/model/UserDao.java
@@ -1,245 +1,51 @@
-package it.unisa.model;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+// Nel costruttore o come variabile statica della classe UserDao
+private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+// Quando si salva un nuovo utente nel database
+public void saveUser(UserBean user) throws SQLException {
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
 
-public class UserDao implements UserDaoInterfaccia {
+    String insertSQL = "INSERT INTO users (username, password) VALUES (?, ?)";
+    try {
+        connection = ds.getConnection();
+        preparedStatement = connection.prepareStatement(insertSQL);
+        preparedStatement.setString(1, user.getUsername());
+        preparedStatement.setString(2, passwordEncoder.encode(user.getPassword()));
+        preparedStatement.executeUpdate();
+    } finally {
+        if (preparedStatement != null) preparedStatement.close();
+        if (connection != null) connection.close();
+    }
+}
 
-	private static DataSource ds;
+// Quando si recupera un utente dal database per il login
+public UserBean doRetrieve(String username, String password) throws SQLException {
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    UserBean user = null;
 
-	static {
-		try {
-			Context initCtx = new InitialContext();
-			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+    String selectSQL = "SELECT * FROM users WHERE username = ?";
+    try {
+        connection = ds.getConnection();
+        preparedStatement = connection.prepareStatement(selectSQL);
+        preparedStatement.setString(1, username);
+        ResultSet rs = preparedStatement.executeQuery();
 
-			ds = (DataSource) envCtx.lookup("jdbc/storage");
-
-		} 
-		catch (NamingException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-	}
-	
-	private static final String TABLE_NAME = "cliente";
-	
-	
-	@Override
-	public synchronized void doSave(UserBean user) throws SQLException {
-		
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		String insertSQL = "INSERT INTO " + UserDao.TABLE_NAME 
-						+ " (NOME, COGNOME, USERNAME, PWD, EMAIL, DATA_NASCITA, CARTA_CREDITO, INDIRIZZO, CAP, AMMINISTRATORE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
-		
-		try {
-			connection = ds.getConnection();
-			connection.setAutoCommit(false);
-			preparedStatement = connection.prepareStatement(insertSQL);
-			preparedStatement.setString(1, user.getNome());
-			preparedStatement.setString(2, user.getCognome());
-			preparedStatement.setString(3, user.getUsername());
-			preparedStatement.setString(4, user.getPassword());
-			preparedStatement.setString(5, user.getEmail());
-			preparedStatement.setDate(6, (Date) user.getDataDiNascita());
-			preparedStatement.setString(7, user.getCartaDiCredito());
-			preparedStatement.setString(8, user.getIndirizzo());
-			preparedStatement.setString(9, user.getCap());
-			preparedStatement.setBoolean(10, user.isAmministratore());
-		
-			preparedStatement.executeUpdate();
-
-			connection.commit();
-		}
-		 finally {
-				try {
-					if (preparedStatement != null)
-						preparedStatement.close();
-				} 
-				finally {
-					if (connection != null)
-						connection.close();
-				}
-		 }
-	}
-
-
-	@Override
-	public synchronized UserBean doRetrieve(String username, String password) throws SQLException {
-		//preparing some objects for connecNon 
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		
-		UserBean user = new UserBean();
-		
-		String searchQuery = "select * from " + UserDao.TABLE_NAME 
-							+ "	where username = ? "
-							+ " AND pwd = ? ";
-		
-		try
-			{
-			//connect to DB 
-			connection = ds.getConnection();
-			preparedStatement = connection.prepareStatement(searchQuery);
-			preparedStatement.setString(1, username);
-			preparedStatement.setString(2, password);
-			ResultSet rs = preparedStatement.executeQuery();
-			boolean more = rs.next();
-			// if user does not exist set the isValid variable to false
-				if (!more) 
-					user.setValid(false);
-		
-				//if user exists set the isValid variable to true
-				else if (more) 
-				{
-					user.setUsername(rs.getString("username"));
-					user.setPassword(rs.getString("pwd"));
-					user.setEmail(rs.getString("email"));
-					user.setNome(rs.getString("nome"));
-					user.setCognome(rs.getString("cognome"));
-					user.setDataDiNascita(rs.getDate("data_nascita"));
-					user.setCartaDiCredito(rs.getString("carta_credito"));
-					user.setIndirizzo(rs.getString("indirizzo"));
-					user.setCap(rs.getString("cap"));
-					user.setAmministratore(rs.getBoolean("amministratore"));
-					user.setValid(true);
-				}
-			}
-			catch (Exception ex) 
-			{
-				System.out.println("Log In failed: An Exception has occurred! " + ex); 
-			}
-			finally {
-				try {
-					if (preparedStatement != null)
-							preparedStatement.close();
-					} 
-			finally {
-				if (connection != null)
-					connection.close();
-			}
-	 }
-
-		return user;
-	}
-	
-	@Override
-	public synchronized ArrayList<UserBean> doRetrieveAll(String order) throws SQLException {
-	
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		ArrayList<UserBean> users = new ArrayList<UserBean>();
-
-		String selectSQL = "SELECT * FROM " + UserDao.TABLE_NAME;
-
-		if (order != null && !order.equals("")) {
-			selectSQL += " ORDER BY " + order;
-		}
-
-		try {
-			connection = ds.getConnection();
-			preparedStatement = connection.prepareStatement(selectSQL);
-
-			ResultSet rs = preparedStatement.executeQuery();
-			
-			while (rs.next()) {
-				UserBean user = new UserBean();
-				user.setUsername(rs.getString("username"));
-				user.setPassword(rs.getString("pwd"));
-				user.setEmail(rs.getString("email"));
-				user.setNome(rs.getString("nome"));
-				user.setCognome(rs.getString("cognome"));
-				user.setDataDiNascita(rs.getDate("data_nascita"));
-				user.setCartaDiCredito(rs.getString("carta_credito"));
-				user.setIndirizzo(rs.getString("indirizzo"));
-				user.setCap(rs.getString("cap"));
-				user.setAmministratore(rs.getBoolean("amministratore"));
-				user.setValid(true);
-				users.add(user);
-			}
-		}
-		finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-			} 
-			finally {
-				if (connection != null)
-					connection.close();
-			}
-		}
-		
-		return users;
-	}
-	
-	public synchronized void doUpdateSpedizione(String email, String indirizzo, String cap) throws SQLException {
-		
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		String updateSQL = "UPDATE " + UserDao.TABLE_NAME
-				+ " SET INDIRIZZO = ?, CAP = ?"
-				+ " WHERE EMAIL = ? ";
-		
-		try {
-			connection = ds.getConnection();
-			connection.setAutoCommit(false);
-			preparedStatement = connection.prepareStatement(updateSQL);
-			preparedStatement.setString(1, indirizzo);
-			preparedStatement.setString(2, cap);
-			preparedStatement.setString(3, email);
-			preparedStatement.executeUpdate();
-
-			connection.commit();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-			} finally {
-				if (connection != null)
-					connection.close();
-			}
-		}
-	}
-	
-	public synchronized void doUpdatePagamento(String email, String carta) throws SQLException {
-		
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		String updateSQL = "UPDATE " + UserDao.TABLE_NAME
-				+ " SET CARTA_CREDITO = ?"
-				+ " WHERE EMAIL = ? ";
-		
-		try {
-			connection = ds.getConnection();
-			connection.setAutoCommit(false);
-			preparedStatement = connection.prepareStatement(updateSQL);
-			preparedStatement.setString(1, carta);
-			preparedStatement.setString(2, email);
-			preparedStatement.executeUpdate();
-
-			connection.commit();
-		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-			} finally {
-				if (connection != null)
-					connection.close();
-			}
-		}
-	}
-
-	}
-
+        if (rs.next()) {
+            String hashedPassword = rs.getString("password");
+            if (passwordEncoder.matches(password, hashedPassword)) {
+                user = new UserBean();
+                user.setUsername(username);
+                user.setPassword(hashedPassword); // Puoi impostare l'hash come password nel bean, o lasciarla vuota per motivi di sicurezza
+                user.setValid(true);
+            }
+        }
+    } finally {
+        if (preparedStatement != null) preparedStatement.close();
+        if (connection != null) connection.close();
+    }
+    return user;
+}
